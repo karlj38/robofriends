4d5bbe5371440674fc8b4de4c1b2b2be04966c3b
Karl Jones <karl.jones@nuom.co.uk>
2025-12-17 13:38:36 +0000

    add Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+export default function Card({ robot: { email, id, name } }) {
+  return (
+    <div className="bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
