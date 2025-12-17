211044ec9d61fd60d04d4cf0bf6a43e84a900787
Karl Jones <karl.jones@nuom.co.uk>
2025-12-17 15:28:26 +0000

    filter robots by search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,31 @@
 import { Component } from "react";
 import { robots } from "../../robots";
 import CardList from "../CardList/CardList";
+import Search from "../Search/Search";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       robots,
+      searchField: "",
     };
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter((robot) =>
+      robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    );
+
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <CardList robots={this.state.robots} />
+        <Search searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
