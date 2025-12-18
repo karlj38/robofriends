6ff7df942d7f29228f25b812dfc4cf09b887ebc0
Karl Jones <karl.jones@nuom.co.uk>
2025-12-18 13:02:33 +0000

    refactor App to functional component with hooks

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,4 +1,4 @@
-import Card from "../Card";
+import { Card } from "../";
 
 export default function CardList({ robots }) {
   if (!robots) throw new Error("No robots");
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,49 +1,40 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { CardList, ErrorBoundary, Search, Scroll } from "../../components";
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+export default function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const { searchField, robots } = this.state;
+  const filteredRobots = robots.filter((robot) =>
+    robot.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
-    const filteredRobots = robots.filter((robot) =>
-      robot.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-
-    return !robots.length ? (
-      <h1 className="f1">Loading...</h1>
-    ) : (
-      <>
-        <header>
-          <h1 className="f1">RoboFriends</h1>
-          <Search searchChange={this.onSearchChange} />
-        </header>
-        <main>
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundary>
-          </Scroll>
-        </main>
-      </>
-    );
-  }
+  return !robots.length ? (
+    <h1 className="f1">Loading...</h1>
+  ) : (
+    <>
+      <header>
+        <h1 className="f1">RoboFriends</h1>
+        <Search searchChange={onSearchChange} />
+      </header>
+      <main>
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </main>
+    </>
+  );
 }
