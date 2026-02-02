166992b081b3981a78ceab00249b759270f442df
Karl Jones <karl.jones@nuom.co.uk>
2026-02-02 16:14:53 +0000

    update card img sizing

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,12 @@
 export default function Card({ robot: { email, id, name } }) {
   return (
     <article className="bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <img
+        alt="robots"
+        height="200"
+        src={`https://robohash.org/${id}?size=200x200`}
+        width="200"
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
