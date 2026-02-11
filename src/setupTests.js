085b646808705f40ebc88660d17b29d739eb292c
Karl Jones <karl.jones@nuom.co.uk>
2026-02-11 19:14:17 +0000

    install and setup msw

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,99 @@
 import { renderWithProviders } from "./utils/test-utils";
 import { page } from "vitest/browser";
+import { http, HttpResponse } from "msw";
+import { setupWorker } from "msw/browser";
 
 page.extend({ renderWithProviders });
+
+//-------------------------------------------------------------------------------------------------
+
+const mockUsers = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+];
+
+// Set up MSW worker with API handlers
+export const httpWorker = setupWorker(
+  http.get("https://jsonplaceholder.typicode.com/users", () =>
+    HttpResponse.json(mockUsers),
+  ),
+  http.get("https://robohash.org/:id", async () => {
+    // Get an ArrayBuffer from reading the file from disk or fetching it.
+    const buffer = await fetch("/icons/192.png").then((response) =>
+      response.arrayBuffer(),
+    );
+
+    return HttpResponse.arrayBuffer(buffer, {
+      headers: {
+        "content-type": "image/png",
+      },
+    });
+  }),
+);
