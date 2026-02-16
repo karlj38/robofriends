b1e29b59367f8bd2d69d94a2d327772f0dc0276b
Karl Jones <karl.jones@nuom.co.uk>
2026-02-16 13:43:47 +0000

    convert src index to TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import setupStore from "./store";
+import store from "./redux/store";
 import "tachyons";
 import "./index.css";
 import { App } from "./containers";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
 root.render(
   <React.StrictMode>
-    <Provider store={setupStore()}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
