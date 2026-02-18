448018cbdde575a5b86cdeb267281cb304ef1a2a
Karl Jones <karl.jones@nuom.co.uk>
2026-02-18 17:44:28 +0000

    fix RTKQ import

---
+++ src/redux/services/RTKQ-fix.d.ts
@@ -0,0 +1,7 @@
+declare module "@reduxjs/toolkit/query/react" {
+  export * from "@reduxjs/toolkit/dist/query/react";
+}
+
+declare module "@reduxjs/toolkit/query" {
+  export * from "@reduxjs/toolkit/dist/query";
+}
