7efd68df456c1813dab10d58ad5282146cbf673a
Karl Jones <karl.jones@nuom.co.uk>
2026-02-18 17:45:34 +0000

    move filterRobots fn to utils

---
+++ src/utils/app-utils.ts
@@ -0,0 +1,12 @@
+import type { Robot } from "../types";
+
+export function filterRobots(
+  robots: Array<Robot> | undefined,
+  searchTerm: string,
+): Array<Robot> {
+  if (!robots) return [];
+
+  return robots.filter((robot) =>
+    robot.name.toLowerCase().includes(searchTerm),
+  );
+}
