a14dccff8a9339274f89350f258a57dc99a71304
Karl Jones <karlj38@gmail.com>
2026-03-16 17:05:55 +0000

    update CardList to click through to Robot page

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,11 +1,21 @@
-import type { Robot } from "../../types";
+import styles from "./CardList.module.css";
 import { Card } from "..";
+import Link from "next/link";
+import type { Robot } from "#/types";
 
 export default function CardList({ robots }: { robots: Array<Robot> }) {
   return (
     <>
       {robots.map((robot) => {
-        return <Card key={robot.id} robot={robot} />;
+        return (
+          <Link
+            key={robot.id}
+            href={`/robots/${robot.id}`}
+            className={styles.cardLink}
+          >
+            <Card key={robot.id} robot={robot} />;
+          </Link>
+        );
       })}
     </>
   );
