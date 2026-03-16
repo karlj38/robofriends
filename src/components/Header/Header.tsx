cc89041535a2a2fa3b042af0058153a027d6090a
Karl Jones <karlj38@gmail.com>
2026-03-16 16:49:26 +0000

    Header to link back to Home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
     <header>
-      <h1 className="f1 sega">RoboFriends</h1>
+      <Link className={styles.headerLink} href="/">
+        <h1 className="f1 sega">RoboFriends</h1>
+      </Link>
     </header>
   );
 }
