3a90a7e6613797541a9b3aa69965b73ace69defc
Karl Jones <karlj38@gmail.com>
2026-04-09 15:24:57 +0100

    fix Vitest Next.js Links

--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -3,6 +3,10 @@ import { playwright } from "@vitest/browser-playwright";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  define: {
+    global: "window",
+    "process.env": JSON.stringify(process.env),
+  },
   plugins: [react()],
   resolve: {
     tsconfigPaths: true,
