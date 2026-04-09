d4d6388e422eaf3a6fb05e54c724a6d466bc47b4
Karl Jones <karlj38@gmail.com>
2026-04-09 15:21:55 +0100

    restore test-utils

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,37 @@
+import React, { PropsWithChildren } from "react";
+import { render } from "vitest-browser-react";
+import type { RenderOptions } from "vitest-browser-react";
+import { Provider } from "react-redux";
+
+import type { AppStore, RootState } from "#/redux/store";
+import { makeStore } from "#/redux/store";
+
+// This type interface extends the default options for render from vitest-browser-react, as well
+// as allows the user to specify other things such as preloadedState, store.
+interface ExtendedRenderOptions extends Omit<RenderOptions, "wrapper"> {
+  // preloadedState?: RootState;
+  store?: AppStore;
+}
+
+export async function renderWithProviders(
+  ui: React.ReactElement,
+  extendedRenderOptions: ExtendedRenderOptions = {},
+) {
+  const {
+    // preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = makeStore(),
+    ...renderOptions
+  } = extendedRenderOptions;
+
+  const Wrapper = ({ children }: PropsWithChildren) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  const screen = await render(ui, { wrapper: Wrapper, ...renderOptions });
+  // Return an object with the store, and the result of rendering
+  return {
+    store,
+    ...screen,
+  };
+}
