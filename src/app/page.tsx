73c54bf6d687017542151e5b3fc85ca570e485a7
Karl Jones <karlj38@gmail.com>
2026-04-09 15:26:12 +0100

    tidy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { CardList, Scroll, Search } from "#/components";
 import type { Robot } from "#/types";
-import { Suspense } from "react";
 
 export default async function HomePage() {
-  // "use cache";
-
   const data = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "force-cache",
   });
